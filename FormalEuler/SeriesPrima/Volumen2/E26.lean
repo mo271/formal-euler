ded0723a4f8a3d777e92ef850bc3ead716d2bfd2
Moritz Firsching <firsching@google.com>
2025-12-18 10:42:33 +0100

    perge E26

--- FormalEuler/SeriesPrima/Volumen2/E26.lean
+++ FormalEuler/SeriesPrima/Volumen2/E26.lean
@@ -152,6 +152,165 @@ theorem fallacia_conjecturae : ¬ (∀ n, (2 ^ (2 ^ n) + 1).Prime) := by
   exact sequens_non_primus
 
 /-!
+Considerabo nunc etiam formulam $2^n - 1$, quae, quoties $n$ non est numerus primus, habet
+divisores, ...
+-/
+theorem si_n_compositus_mersennus_compositus (n : ℕ) (h : ¬ n.Prime) (hn : 1 < n) :
+    ¬ (2 ^ n - 1).Prime := by
+  -- TODO(firsching)
+  refine (Nat.not_prime_iff_exists_dvd_ne ?_).mpr ?_
+  · exact (2).le_pred_of_lt (lt_self_pow₀ (by decide) hn)
+  · have : ∃ x > 1, ∃ y > 1, x < n ∧ y < n ∧ n = x * y := by
+      sorry
+    obtain ⟨x, hx1, y, hy1, hxn, hyn, h_prod⟩ := this
+    use 2^x - 1
+    sorry
+
+
+/-!
+... neque tantum $2^n - 1$, sed etiam $a^n - 1$.
+-/
+theorem si_n_compositus_compositus (n a : ℕ) (h : ¬ n.Prime) (hn : 1 < n) (ha : 1 < a):
+    ¬ (a ^ n - 1).Prime := by
+  -- TODO(firsching)
+  refine (Nat.not_prime_iff_exists_dvd_ne ?_).mpr ?_
+  · exact (Nat.lt_pow_self ha).le_pred.trans' hn
+  · have : ∃ x > 1, ∃ y > 1, x < n ∧ y < n ∧ n = x * y := by
+      sorry
+    obtain ⟨x, hx1, y, hy1, hxn, hyn, h_prod⟩ := this
+    use a^x - 1
+    sorry
+
+
+/-!
+Sed si $n$ sit numerus primus, videri posset etiam $2^n - 1$ semper talem exhibere: hoc tamen
+asseverare nemo est ausus, cum tam facile potuisset refelli.
+-/
+theorem non_omnis_mersennus_primus : ¬ (∀ n, n.Prime → (2 ^ n - 1).Prime) := by
+  push_neg
+  use 11
+  norm_num
+
+/-!
+Namque $2^{11} - 1$ i. e. $2047$ divisores habet $23$ ...
+-/
+theorem exm : (2 ^ 11 - 1).primeFactors = {23, 89} := by
+  simp [Nat.primeFactors]
+
+/-!
+... et $89$ et $2^{23} - 1$ dividi potest per $47$.
+-/
+example : 47 ∣ (2 ^ 23 - 1) := by norm_num
+
+/-!
+Video autem Cel. Wolfium non solum hoc in Elem. Matheseos editione altera non advertisse, ubi
+numeros perfectos investigat atque $2047$ inter primos numerat, sed etiam $511$ seu $2^9 - 1$
+pro tali habet, cum tamen sit divisibilis per $2^3 - 1$ i.e. $7$.
+-/
+theorem errores_wolfii : ¬ (2047).Prime ∧ ¬ (511).Prime ∧ (2^3 - 1) ∣ (2^9 - 1) := by
+  norm_num
+
+/-!
+Dat autem $2^{n-1}(2^n - 1)$ numerum perfectum, quoties $2^n - 1$ est primus, ...
+-/
+theorem generatio_perfecti (n : ℕ) (h_prime : (2 ^ n - 1).Prime) :
+    ((2 ^ (n - 1) * (2 ^ n - 1))).Perfect := by
+  constructor
+  -- TODO(firsching)
+  · sorry
+  · exact (Nat.mul_pos (n - 1).two_pow_pos) (h_prime).pos
+
+/-!
+... debet ergo etiam $n$ esse numerus primus.
+-/
+theorem necessitas_exponentis_primi (n : ℕ) (h_prime : (2 ^ n - 1).Prime) (hn : n ≠ 1) :
+    n.Prime := by
+  by_contra h_not_prime
+  absurd h_prime
+  apply si_n_compositus_mersennus_compositus n
+  · exact h_not_prime
+  · exact hn.symm.lt_of_le (by cases n with | zero => contradiction | succ n => omega)
+
+/-!
+Inveni autem hoc semper fieri, si sit $n = 4m - 1$, atque $8m - 1$ fuerit numerus primus,
+tum enim $2^n - 1$ semper poterit dividi per $8m - 1$.
+-/
+theorem regula_divisoris_mersenni (m n : ℕ) (hm : m ≥ 1) (hn : n = 4 * m - 1)
+    (h_prime : (8 * m - 1).Prime) : (8 * m - 1) ∣ (2 ^ n - 1) := by
+  let p := 8 * m - 1
+  have : n = (p - 1) / 2 := by
+    omega
+  have : Fact (p.Prime) := { out := h_prime }
+  have : 2^n = ((legendreSym p 2) : ZMod p) := by
+    simp only [legendreSym.eq_pow, Int.cast_ofNat]
+    exact (congr_arg _) (by valid)
+  have : 2^n = (1 : ZMod p) := by
+    rw [this,legendreSym.at_two]
+    · rw [ZMod.χ₈_nat_eq_if_mod_eight]
+      norm_num [(by omega : p%2 ≠ 0 ∧ p%8 = 7)]
+    · omega
+  field_simp only [push_cast, sub_self, this, ←CharP.cast_eq_zero_iff (ZMod p),Nat.cast_pred]
+
+
+/-!
+Hinc excludendi sunt casus sequentes: 11, 23, 83, 131, 179, 191, 239, etc. qui numeri pro $n$
+substituti reddunt $2^n - 1$ numerum compositum.
+-/
+theorem casus_excludendi :
+    ¬ (2 ^ 11 - 1).Prime ∧
+    ¬ (2 ^ 23 - 1).Prime ∧
+    ¬ (2 ^ 83 - 1).Prime ∧
+    ¬ (2 ^ 131 - 1).Prime ∧
+    ¬ (2 ^ 179 - 1).Prime ∧
+    ¬ (2 ^ 191 - 1).Prime ∧
+    ¬ (2 ^ 239 - 1).Prime := by
+  norm_num
+
+/-!
+Neque tamen reliqui numeri primi omnes loco $n$ positi satisfaciunt, sed plures insuper
+excipiuntur; sic observavi $2^{37} - 1$ dividi posse per $223$...
+-/
+theorem divisio_37 : 223 ∣ (2 ^ 37 - 1) := by norm_num
+
+/-!
+... $2^{43} - 1$ per $431$...
+-/
+theorem divisio_43 : 431 ∣ (2 ^ 43 - 1) := by norm_num
+
+/-!
+... $2^{29} - 1$ per $1103$...
+-/
+theorem divisio_29 : 1103 ∣ (2 ^ 29 - 1) := by norm_num
+
+/-!
+... $2^{73} - 1$ per $439$, omnes tamen excludere non est in potestate.
+-/
+theorem divisio_13 : 439 ∣ (2 ^ 73 - 1) := by norm_num
+
+/-!
+Attamen asserere audeo praeter hos casus notatos, omnes numeros primos minores quam 50, et forte
+quam 100, efficere $2^{n-1}(2^n - 1)$ esse numerum perfectum, sequentibus numeris pro $n$ positis,
+1, 2, 3, 5, 7, 13, 17, 19, 31, 41, 47, unde 11 proveniunt numeri perfecti.
+-/
+private def indices_euleri : List ℕ := [1, 2, 3, 5, 7, 13, 17, 19, 31, 41, 47]
+
+example : indices_euleri.length = 11 := by decide
+
+/-!
+[Eulerus hic errat: 41 et 47 numeros compositos generant!].
+-/
+theorem refutatio_assertionis_de_41_et_47 :
+    ¬ (2 ^ 41 - 1).Prime ∧ ¬ (2 ^ 47 - 1).Prime := by norm_num
+
+theorem assertio_euleri : ∀ n ∈ indices_euleri, (n ∈ [1, 41, 47] ∨ (2 ^ n - 1).Prime) := by
+  unfold indices_euleri
+  intro n h
+  simp only [List.mem_cons, List.not_mem_nil, or_false] at h
+  rcases h with rfl | rfl | rfl | rfl | rfl | rfl | rfl | rfl | rfl | rfl | rfl | rfl <;> norm_num
+
+
+
+/-!
 ## Theorema 1
 
 Si fuerit $n$ numerus primus, omnes potentia exponentis $n-1$ per $n$ divisa vel nihil vel unitatem
